3f63238d0735b967155748c2b68be36b56316861
TakumuIseki <t-iseki@kawahara.ac.jp>
2025-12-16 18:38:04 +0900

    タイトルシーンのUI配置とボタンによるシーン遷移

---
+++ Assets/Script/TitleSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// タイトルシーンマネージャー
+/// </summary>
+public class TitleSceneManager : MonoBehaviour
+{
+    [Header("ゲームスタートボタン"), SerializeField]
+    Button _gameStartButton;
+
+    /// <summary>
+    /// Awake
+    /// </summary>
+    private void Awake()
+    {
+        // ゲームスタートボタンのリスナー登録
+        _gameStartButton.onClick.AddListener(OnClickGameStartButton);
+    }
+
+    /// <summary>
+    /// ゲームスタートボタン押下時
+    /// </summary>
+    private void OnClickGameStartButton()
+    {
+        // インゲームに遷移
+        SceneManager.LoadScene("InGame");
+    }
+}
