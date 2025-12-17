ca3655422e53c502622c1261158839464d74bfd9
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2025-12-17 10:17:58 +0900

    複数のボタンでそれぞれ別のキャラクターを出す処理の実装
    # Conflicts:
    #	Assets/Prefab/Tank.prefab
    #	Assets/Prefab/Tank.prefab.meta
    #	Assets/Scenes/InGame.unity
    #	Assets/Script/CharacterSummonScript.cs
    #	Assets/Script/GameManagerScript.cs
    #	Assets/Script/HPScript.cs
    #	Assets/Script/SummonButtonScript.cs

---
+++ Assets/Script/SummonButtonScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SummonButtonScript : MonoBehaviour
+{
+    Button summonButton;
+
+    [SerializeField] GameObject CharacterSummon;
+
+    CharacterSummonScript characterSummonScript;
+
+    public int characterId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SummonButton()
+    {
+        summonButton = GetComponent<Button>();
+        characterSummonScript = CharacterSummon.GetComponent<CharacterSummonScript>();
+        summonButton.onClick.AddListener(() =>
+        {
+            if (characterSummonScript.isSummoned[characterId] == false)
+            {
+                StartCoroutine(characterSummonScript.SummonCharacter(characterId));
+            }
+        });
+    }
+}
