38314e36e8ee05b5c9de5e677aa4bc13c1a2ee7f
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2025-12-17 10:46:14 +0900

    複数のボタンでそれぞれ別のキャラクターを出す処理の実装
    # Conflicts:
    #	Assets/Script/HPScript.cs

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] float speed = 0.0f;
+    private Rigidbody2D movePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePlayer = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        if(movePlayer != null)
+        {
+            movePlayer.velocity = new Vector2(speed, movePlayer.velocity.y);
+        }
+    }
+}
