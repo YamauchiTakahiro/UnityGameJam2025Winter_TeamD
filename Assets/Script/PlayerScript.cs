e1742422a1ea12cbc3641a9a375a421f0b5c8a28
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2025-12-17 10:46:14 +0900

    キャラクターの移動の実装

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    //[SerializeField] float speed = 0.0f;
+    [SerializeField] float speed = 0.0f;
+    private Rigidbody2D movePlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        if(movePlayer != null)
+        {
+            movePlayer.velocity = new Vector2(speed, movePlayer.velocity.y);
+        }
     }
 }
