98f4c15cdb6aa59db584aeb55f6ad0e2680c5e6e
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2025-12-17 10:46:14 +0900

    キャラクターの移動の実装
    # Conflicts:
    #	Assets/Prefab/Cat.prefab
    #	Assets/Script/CastleScript.cs
    #	Assets/Script/GameManagerScript.cs
    #	ProjectSettings/Physics2DSettings.asset

--- Assets/Script/CastleScript.cs
+++ Assets/Script/CastleScript.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CastleScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
-    [SerializeField] int castleHealth = 1000;
+    [SerializeField] float speed = 0.0f;
+    private Rigidbody2D movePlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        if(movePlayer != null)
+        {
+            movePlayer.velocity = new Vector2(speed, movePlayer.velocity.y);
+        }
     }
 }
