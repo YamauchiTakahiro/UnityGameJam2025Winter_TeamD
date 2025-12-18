94091d7f51bdbd7924b2870aa0854b3bb59ddad8
KaechiKensuke <CA1254005@st.kawahara.ac.jp>
2025-12-18 15:17:25 +0900

    全体的なUIの設置(仮)
    # Conflicts:
    #	Assets/Scenes/InGame.unity
    #	Assets/Script/Wallet.cs

---
+++ Assets/Script/Wallet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallet : MonoBehaviour
+{
+    /** テキストを宣言するところ */
+    [SerializeField] Text coinText;
+
+    int maxCoin = 10000; // 最大所持金額
+    float coin  = 0; // 所持金額
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText.text = coin.ToString() + "/" + maxCoin.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coin <= maxCoin)
+        {
+            //時間経過でコインが増える
+            coin += Time.deltaTime;
+            coinText.text = coin.ToString() + "/" + maxCoin.ToString();
+        }
+    }
+}
