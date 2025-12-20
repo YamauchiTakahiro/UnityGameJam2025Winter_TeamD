ee38644b270145bb952fa694062734d80501e90e
KaechiKensuke <CA1254005@st.kawahara.ac.jp>
2025-12-20 21:12:44 +0900

    お金の追加

--- Assets/Script/ButtonData.cs
+++ Assets/Script/ButtonData.cs
@@ -1,16 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonData : MonoBehaviour
 {
+    Wallet wallet => Wallet.instance;
+
     [SerializeField] SpriteRenderer player;
+
+    [SerializeField] int cost;
+    [SerializeField] Text costText;
+
+    Button button => GetComponent<Button>();
+    bool isclicked = false;
+    //プレハブを入れる変数
     [SerializeField] GameObject playerPrefab;
+
+    [SerializeField] Slider gaugeBar;
+
+    private void Start()
+    {
+        //価格表示
+        costText.text = cost.ToString() + "円";
+    }
+    private void Update()
+    {
+        //所持金が足りないときはボタンを押せなくする
+        if (!isclicked && wallet.coin >= cost)
+        {
+            //ボタンを押せるようにする
+            button.interactable = true;
+        }
+        else
+        {
+            //ボタンを押せないようにする
+            button.interactable = false;
+        }
+    }
     //押したときに呼ばれる関数
     public void OnClick()
     {
+        //所持金を減らす
+        wallet.coin -= cost;
         //Playerの召喚
         PlayerSpawn();
+
+        //ボタンを押したフラグを立てる
+        isclicked = true;
+        //ゲージの更新
+        StartCoroutine(SliderUpdate()); 
     }
     void PlayerSpawn()
     {
@@ -25,6 +64,27 @@ public class ButtonData : MonoBehaviour
 
         SpriteRenderer pl = obj.GetComponent<SpriteRenderer>();
         pl.sortingOrder = (int)(-y * 10);
-    } 
+    }
+    
+    IEnumerator SliderUpdate()
+    {
+        //ゲージを表示
+        gaugeBar.value = 0;
+        gaugeBar.gameObject.SetActive(true);
 
-   }
+        //時間経過でゲージを増やす
+        while (gaugeBar.value < gaugeBar.maxValue)
+        {
+            gaugeBar.value++;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        //ゲージを非表示
+        gaugeBar.gameObject.SetActive(false);
+
+        //ボタンを押したフラグを下ろす
+        isclicked = false;
+
+    }
+
+}
--- Assets/Script/Wallet.cs
+++ Assets/Script/Wallet.cs
@@ -5,25 +5,37 @@ using UnityEngine.UI;
 
 public class Wallet : MonoBehaviour
 {
+    public static Wallet instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
     /** テキストを宣言するところ */
     [SerializeField] Text coinText;
 
-    int maxCoin = 10000; // 最大所持金額
-    float coin  = 0; // 所持金額
+    public int coinLevel;
+    [SerializeField] int[] maxCoin; // 最大所持金額
+    public float coin  = 0; // 所持金額
+
+   public int coinSpeed = 6; // コインの増える速度
     // Start is called before the first frame update
     void Start()
     {
-        coinText.text = coin.ToString() + "/" + maxCoin.ToString();
+        coinText.text = coin.ToString() + "/" + maxCoin[coinLevel].ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (coin <= maxCoin)
+        if (GameManagerScript.instance.isGame && coin <= maxCoin[coinLevel])
         {
-            //時間経過でコインが増える
-            coin += Time.deltaTime;
-            coinText.text = coin.ToString() + "/" + maxCoin.ToString();
+            //時間経過でコインが増える 
+            coin += Time.deltaTime * coinSpeed;
+            coinText.text = coin.ToString("F0") + "/" + maxCoin[coinLevel].ToString();
         }
     }
 }
